42b21b532dc0dccc8b74f527274870974b9079c1
AIOS <63643735+aranzaortega@users.noreply.github.com>
2023-02-03 14:50:01 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -6,20 +6,10 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Hey, this is Aran <span className="heart">♥️</span>
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Welcome to my online resume
         </p>
       </header>
     </div>
